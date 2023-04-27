9142c66dcd88d0c439869401a2993b2bc6e963de
vladm747 <vladyslav.moroz105@gmail.com>
2023-04-28 00:43:38 +0300

    +startup

--- BooksApp/Startup/Program.cs
+++ BooksApp/Startup/Program.cs
@@ -2,10 +2,8 @@ using BooksApp.Startup;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.RegisterApplicationServices();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
 var app = builder.Build();
